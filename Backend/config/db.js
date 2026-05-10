d9f84fbe394666d9590612c62a9096f0ecca7825
Aryan@3001 <aryanchauhan9424@gmail.com>
2026-05-10 14:55:23 +0530

    Fix: Server.js file

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI) // match what's on Render;
+    const conn = await mongoose.connect(process.env.MONGO_URI) // match what's on Render;
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,7 @@ app.use(cors({
   credentials: true,
 }));
 
-app.options("*", cors()); // ✅ handles preflight requests
+
 
 app.use(express.json());
 
